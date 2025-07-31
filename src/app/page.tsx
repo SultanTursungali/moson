cb5b2bfc27fb9ac3bd6e1e67ca0c61848e951276
Sultan <sultan250t@gmail.com>
2025-07-31 13:11:02 +0500

    Fifth Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import Info from "./components/Info";
-import LodgesSection from "./components/LodgesSection";
-import MainLodges from "./components/MainLodges"; 
 import Values from "./components/Values";
 import Readers from "./components/Readers";
 import Lodges from "./components/Lodges";
@@ -14,8 +12,6 @@ export default function HomePage() {
       <Info />
       <Readers />
       <Values/>
-      {/* <MainLodges />
-      <LodgesSection /> */}
       <Lodges/>
     </main>
   );
